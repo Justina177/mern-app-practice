a94bc315b2647541aad0e806572fde7a434a768c
justina177 <justinaenenche@yahoo.com>
2023-04-19 19:24:55 +0100

    Passing props from Content1.js to ItemList.js( prop drilling)

--- src/Content1.js
+++ src/Content1.js
@@ -1,34 +1,20 @@
 import React from 'react';
-import { FaTrashAlt } from 'react-icons/fa';
+import ItemList from './ItemList';
 
 const Content1 = ({items, handleCheck, handleDelete}) => {   
     
     return (
         <main className='main'>
             {items.length ? (
-                <ul>
-                    {items.map((item) => (
-                        <li className='item' key={item.id}>
-                            <input
-                                type="checkbox"
-                                onChange={() => handleCheck(item.id)}
-                                checked={item.checked}
-                            />
-                            <label>{item.item}</label>
-                            <FaTrashAlt 
-                            onClick={() => handleDelete(item.id)}
-                            role="button" 
-                            tabIndex="0"
-                            />
-                        </li>
-                    ))}
-                </ul>
+                <ItemList 
+                items={items}
+                handleCheck={handleCheck}
+                handleDelete={handleDelete}
+                />  
             ) : (
                 <p style={{ marginTop: '2rem' }}>Your list is Empty.</p>
             )}
-
-            {/* lesson 7 time 8.00  npm i react-icons -D  */}
-            
+            {/* lesson 7 time 8.00  npm i react-icons -D  */}         
         </main>
     )
 }
