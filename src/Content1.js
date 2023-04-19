493187f6c603d2f64d8694a09eaaf31321b61f2d
justina177 <justinaenenche@yahoo.com>
2023-04-19 16:29:24 +0100

    Added code for if items = 0/ that is length.array=0

--- src/Content1.js
+++ src/Content1.js
@@ -44,23 +44,27 @@ const Content1 = () => {
    
   return (
     <main className='main'>
-        <ul>
-            {items.map((item) => (
-                <li className='item' key={item.id}>
-                    <input
-                        type="checkbox"
-                        onChange={() => handleCheck(item.id)}
-                        checked={item.checked}
-                    />
-                    <label>{item.item}</label>
-                    <FaTrashAlt 
-                    onClick={() => handleDelete(item.id)}
-                    role="button" 
-                    tabIndex="0"
-                    />
-                </li>
-            ))}
-        </ul>
+        {items.length ? (
+            <ul>
+                {items.map((item) => (
+                    <li className='item' key={item.id}>
+                        <input
+                            type="checkbox"
+                            onChange={() => handleCheck(item.id)}
+                            checked={item.checked}
+                        />
+                        <label>{item.item}</label>
+                        <FaTrashAlt 
+                        onClick={() => handleDelete(item.id)}
+                        role="button" 
+                        tabIndex="0"
+                        />
+                    </li>
+                ))}
+            </ul>
+        ) : (
+            <p style={{ marginTop: '2rem' }}>Your list is Empty.</p>
+        )}
 
         {/* lesson 7 time 8.00  npm i react-icons -D  */}
         
