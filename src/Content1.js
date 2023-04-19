76be1e845d9f8ac332c4f16771fb08b9a8adfb7e
justina177 <justinaenenche@yahoo.com>
2023-04-19 11:08:30 +0100

    Added useState hook to change the state of the app

--- src/Content1.js
+++ src/Content1.js
@@ -1,35 +1,61 @@
 import React from 'react';
-import { useState } from 'react'
+import { useState } from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
+
 
 const Content1 = () => {
-    const [name, setName] = useState('Tina');
-    const [count, setCount] = useState(0);
+    const [items, setItems] = useState([
+        {
+            id: 1,
+            checked: false,
+            item: "One Bag of Rice"
+        },
+        {
+            id: 2,
+            checked: false,
+            item: "Five Kilo of Meat"
+        },
+        {
+            id: 3,
+            checked: false,
+            item: "Item 3"
+        },
+        {
+            id: 4,
+            checked: false,
+            item: "Milk"
+        }
+    ]);
 
-    const handleNameChange = () => {
-        const names = ["Bob", "Kevin", "Tina"];
-        const int = Math.floor(Math.random() * 3);
-        setName(names[int]);
-      }
-
-      const handleClick = () => {
-          setCount(count +1)
-          setCount(count +1)
-          console.log(count)
-      }
-
-      const handleClick2 = (name) => {
-          console.log(count)
-      }
+    const handleCheck = (id) => {
+        const listItems = items.map((item) => item.id === id ? { ...item,
+        checked: !item.checked } : item);
+        setItems(listItems);
+    }
 
 
+   
   return (
     <main className='main'>
-        <p onDoubleClick={handleClick}>
-            Hello {name}!
-        </p>
-        <button onClick={handleNameChange}>Change Name</button>
-        <button onClick={handleClick}>Click It</button>
-        <button onClick={handleClick2}>Click Here</button>
+        <ul>
+            {items.map((item) => (
+                <li className='item' key={item.id}>
+                    <input
+                        type="checkbox"
+                        onChange={() => handleCheck(item.id)}
+                        checked={item.checked}
+                    />
+                    <label>{item.item}</label>
+                    <FaTrashAlt 
+                    role="button" 
+                    tabIndex="0"
+                    />
+                </li>
+            ))}
+        </ul>
+
+        {/* lesson 7 time 8.00  npm i react-icons -D  */}
+        
     </main>
   )
 }
