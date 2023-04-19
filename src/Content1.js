ec5fa2385329c04d8315abd77a58eac69e57fdfc
justina177 <justinaenenche@yahoo.com>
2023-04-19 03:45:42 +0100

    Anomynous fuction added

--- src/Content1.js
+++ src/Content1.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Content1 = () => {
     const handleNameChange = () => {
-        const names = ["Bob", "Kevin", "Dave"];
+        const names = ["Bob", "Kevin", "Tina"];
         const int = Math.floor(Math.random() * 3);
         return names[int];
       }
@@ -10,10 +10,14 @@ const Content1 = () => {
       const handleClick = () => {
           console.log('Button is being Clicked')
       }
+      const handleClick2 = (name) => {
+          console.log(`${name} was clicked`)
+      }
   return (
     <main className='main'>
         <p>Hello {handleNameChange()}!</p>
         <button onClick={handleClick}>Click Here</button>
+        <button onClick={() => handleClick2('Tina')}>Click It</button>
     </main>
   )
 }
