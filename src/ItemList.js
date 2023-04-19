d6844e2cb3d0198841158e8c9df43004f8ff146d
justina177 <justinaenenche@yahoo.com>
2023-04-19 19:42:57 +0100

    Added one more prop drilling from ItemList component to LineItem component

--- src/ItemList.js
+++ src/ItemList.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import { FaTrashAlt } from 'react-icons/fa';
+import LineItem from './LineItem';
+// import { FaTrashAlt } from 'react-icons/fa';
 
 const ItemList = ({ items, handleCheck, handleDelete }) => {
   return (
     <ul>
         {items.map((item) => (
-            <li className='item' key={item.id}>
-                <input
-                    type="checkbox"
-                    onChange={() => handleCheck(item.id)}
-                    checked={item.checked}
-                />
-                <label>{item.item}</label>
-                <FaTrashAlt 
-                onClick={() => handleDelete(item.id)}
-                role="button" 
-                tabIndex="0"
-                />
-            </li>
+            <LineItem 
+            key={item.id}
+            item={item}
+            handleCheck={handleCheck}
+            handleDelete={handleDelete}
+            />
+            
         ))}
     </ul>
   )
